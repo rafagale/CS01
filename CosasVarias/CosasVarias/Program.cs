47c30e41355f2150d360dcc3e37051e7f176b4d4
Rafa <raf141.92@gmail.com>
2018-05-30 20:29:33 +0200

    30-05

---
+++ CosasVarias/CosasVarias/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosasVarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+        }//Main
+
+        public static void Hablar(string frase)
+        {
+            SpeechSynthesizer hablar = new SpeechSynthesizer();
+        pedir:
+            try
+            {
+                Console.WriteLine("Introduce un numero");
+                frase = Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                goto pedir;
+            }
+        }//Hablar
+
+        public static int pedirEntero(string frase)
+        {
+            int numero = 0;
+        pedirEntero:
+            try
+            {
+                Console.WriteLine(frase);
+                numero = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                goto pedirEntero;
+            }
+            return numero;
+
+        }//PedirEntero
+
+    }//Class
+
+}//namespace

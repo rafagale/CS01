47cd0b14c571a4715cd208ecaa2203d369606828
Rafa <raf141.92@gmail.com>
2018-06-03 19:57:14 +0200

    re

---
+++ Recursivos/Recursivos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }//main
+
+        public static int Minimo(int[] numeros, int minimoInf, int minimoSup)
+        {
+            if (minimoInf == minimoSup)
+                return numeros[minimoSup];
+            if (numeros[minimoInf] > numeros[minimoSup])
+                return Minimo(numeros, minimoInf + 1, minimoSup);
+            else return Minimo(numeros, minimoInf, minimoSup - 1);
+        }//Minimo
+
+
+        public static int sumarDigitos(int num)
+        {
+            if (num == 0)
+            {
+                return num;
+            }
+            else
+            {
+                return num % 10 + sumarDigitos(num / 10);
+            }
+        }//sumarDigitos
+
+    }//Class
+
+}//namespace
---
+++ Recursivos/Recursivos/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Recursivos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Recursivos")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("56aae3da-d058-4ed3-a74e-fc4c6c0a9eb9")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
